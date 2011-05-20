a9bf3eb32e52ac0384b1ea6edbe3df9f11442beb
Chip Castle <chip@chipcastle.com>
2011-05-20 07:57:47 -0500

    Sending html content email with Pony, Beta welcome message looks good, show error if form isn't filled out

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 gem 'activerecord', '3.0.5'
 gem 'mysql2'
+gem 'pony'
--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'aasm'
 require File.join(File.dirname(__FILE__), 'database')
+require 'pony'
 
 get '/' do
   @users = User.all
@@ -13,5 +14,21 @@ get '/users/beta/new' do
 end
 
 post '/users/beta' do
-  params.inspect
+  @name, @email, @password = params[:name], params[:email], params[:password]
+  if params_ok?
+    Pony.mail :to       => @email,
+              :bcc      => 'chip@invoicethat.com',
+              :from     => 'chip@invoicethat.com',
+              :subject  => 'Welcome to invoicethat.com',
+              :body     => haml(:beta_welcome),
+              :content_type => 'text/html'
+    "An email was sent to #{@name} with email #{@email} and password #{@password}"
+  else
+    #params.inspect
+    "Dude, you forgot to fill out the form completely"
+  end
+end
+
+def params_ok?
+  @name != '' && @email != '' && @password != ''
 end
